b25a12c983796531b130df5870fcdbd90d27c770
tanakakunitoshi <tanaka.kunitoshi@cloud-ace.jp>
2020-08-27 13:17:21 +0900

    #1 SOOの部分をJICFSに変更

--- sql/Procedure/denormalization.sql
+++ sql/Procedure/denormalization.sql
@@ -8,12 +8,12 @@ BEGIN
     SELECT
       tran.* EXCEPT(office,member,jan_code,dummy),
       master_member.* EXCEPT(dummy),
-      master_jan.* EXCEPT(item_code,maker_code,brand_code,SOO_item,dummy),
+      master_jan.* EXCEPT(item_code,maker_code,brand_code,JICFS_item,dummy),
       master_item.* EXCEPT(dummy),
       master_office.* EXCEPT(dummy),
       master_maker.* EXCEPT(dummy),
       master_brand.* EXCEPT(dummy),
-      master_SOO_item.* EXCEPT(dummy)
+      master_JICFS_item.* EXCEPT(dummy)
     FROM
       `looker.transaction` AS tran
     LEFT JOIN
@@ -40,7 +40,7 @@ BEGIN
     ON
       master_jan.brand_code = master_brand.brand_code
     LEFT JOIN
-      `looker.SOO_item` AS master_SOO_item
+      `looker.JICFS_item` AS master_JICFS_item
     ON
-      master_jan.SOO_item = master_SOO_item.soo_code;
+      master_jan.JICFS_item = master_JICFS_item.JICFS_code;
 END;    
